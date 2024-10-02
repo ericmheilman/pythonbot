8909c0d4445e3a999719b5811438fc340a274458
ericheilman <ericmheilman@gmail.com>
2024-10-02 02:25:34 -0400

    clean

--- git-filter.sh
+++ git-filter.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 git filter-branch --force --index-filter \
-"git rm --cached --ignore-unmatch bots/iterate-algo.py" \
+"git rm --cached --ignore-unmatch scripts/API-KEY.sh:2" \
 --prune-empty --tag-name-filter cat -- --all
